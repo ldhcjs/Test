95efbd948bce05225c268268e4ef4b3acadb7704
Tony <ldhcjs@gmail.com>
2021-01-05 00:32:24 +0900

    Test change

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tv1)
     }
 }
